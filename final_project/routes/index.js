7376b75141bbb59de87fd7a08486c289fdd88506
sunny <sunnyding602@qq.com>
2016-11-10 14:35:16 -0500

    the skeleton of our final project is made

--- final_project/routes/index.js
+++ final_project/routes/index.js
@@ -1,11 +1,19 @@
+const userRoutes = require("./user");
 
 const constructorMethod = (app) => {
 
-    app.use("*", (req, res) => {
-        res.render("examples/home", {
+    app.use("/users", userRoutes);
+    
+    app.use("/", (req, res) => {//home page, you can also call it index.html
+        res.render("home", {
             partial: "home-scripts"
         });
     })
+
+    app.use("*", (req, res) => {
+        res.sendStatus(404);
+    })
+
 };
 
 module.exports = constructorMethod;
